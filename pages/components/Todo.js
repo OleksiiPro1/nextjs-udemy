1d040c9438df3169bdbcd48d6004c95a1d63397b
Alexey Piltenko <101993475+OleksiiPro1@users.noreply.github.com>
2022-10-13 13:34:18 +0200

    props, useState

--- pages/components/Todo.js
+++ pages/components/Todo.js
@@ -1,12 +1,34 @@
+import { useState } from 'react';
+import Backdrop from './Backdrop';
+import Modal from './Modal';
+
 export default function Todo(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  function closeHandler() {
+    setShowModal(false);
+  }
+
+  function deleteHandler() {
+    // console.log(props.text, 'Clicked!');
+    setShowModal(true);
+  }
+
   return (
     <>
       <div></div>
       <div className="card">
         <h2>{props.text}</h2>
         <div className="actions">
-          <button className="btn">Delete</button>
+          <button className="btn" onClick={deleteHandler}>
+            Delete
+          </button>
         </div>
+
+        {showModal && (
+          <Modal onCancel={closeHandler} onConfirm={closeHandler} />
+        )}
+        {showModal && <Backdrop onCancel={closeHandler} />}
       </div>
     </>
   );
